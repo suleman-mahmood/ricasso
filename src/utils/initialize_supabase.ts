83c4aa26c6e0433340e7fb66982b887b8c22c77a
Suleman Mahmood <sulemanmahmood99@gmail.com>
2024-04-07 03:27:24 +0500

    initialized supabase

---
+++ src/utils/initialize_supabase.ts
@@ -0,0 +1,3 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient("https://ouifihuvgjvgvqiqrrzp.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91aWZpaHV2Z2p2Z3ZxaXFycnpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI0MzkwNjYsImV4cCI6MjAyODAxNTA2Nn0.KqWJPEExOr86BI6IVc7Km_mdDo_gKiZL6fJY0L0a2XA");
