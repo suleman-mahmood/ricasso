cdfe3ee3fde1a14c2fb9b44fa1c18713ef5bc464
Suleman Mahmood <sulemanmahmood99@gmail.com>
2024-04-21 15:29:24 +0500

    made close order btn more friendly

--- src/app/user/trade/page.tsx
+++ src/app/user/trade/page.tsx
@@ -264,7 +264,7 @@ export default function SignupFormDemo() {
                   <TableCell>{invoice.unrealizedProfitLoss}</TableCell>
                   <TableCell className="text-right">{invoice.amount}</TableCell>
                   <TableCell className="text-right">
-                    <button className="inline-flex h-12 items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#4d0000,55%,#4d0000)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                    <button className="btn btn-error">
                       Close order
                     </button>
                   </TableCell>
